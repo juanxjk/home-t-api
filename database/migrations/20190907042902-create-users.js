17473575e3c3a062b3d6191ee77c4a28a367073f
Juan Carlos <juanksoliveira@gmail.com>
2019-09-07 01:55:04 -0300

    Add migration script and migration create-users

--- database/migrations/20190907042902-create-users.js
+++ database/migrations/20190907042902-create-users.js
@@ -1,23 +1,41 @@
 "use strict";
 
+// created by command: sequelize migration:create --name=create-users
+
 module.exports = {
     up: (queryInterface, Sequelize) => {
-        /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
+        return queryInterface.createTable("Users", {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: DataTypes.INTEGER
+            },
+            name: {
+                allowNull: false,
+                type: DataTypes.STRING
+            },
+            email: {
+                allowNull: false,
+                type: DataTypes.STRING,
+                unique: true
+            },
+            password: {
+                allowNull: false,
+                type: DataTypes.STRING
+            },
+            createdAt: {
+                allowNull: false,
+                type: DataTypes.DATE
+            },
+            updatedAt: {
+                allowNull: false,
+                type: DataTypes.DATE
+            }
+        });
     },
 
     down: (queryInterface, Sequelize) => {
-        /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+        return queryInterface.dropTable("Users");
     }
 };
