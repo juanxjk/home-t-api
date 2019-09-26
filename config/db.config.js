c0726610651641fc930141c144cc29490ef4991a
Juan Carlos <juanksoliveira@gmail.com>
2019-09-26 01:58:36 -0300

    Update sequelize config file

---
+++ config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    host: "localhost",
+    username: "postgres",
+    password: "docker",
+    database: "home_t",
+    dialect: "postgres",
+    operatorsAliases: false,
+    logging: false,
+    define: {
+        timestamps: true,
+        underscoredAll: true
+    }
+};
