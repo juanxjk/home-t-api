c09c0fc2b0c91e634e011d7fc4165c444b5eabed
Juan Carlos <juanksoliveira@gmail.com>
2019-10-01 01:33:06 -0300

    Update format with eslint rules

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const config = require("../../config/db.config.js");
 const db = {};
@@ -14,20 +14,23 @@ const sequelize = new Sequelize(
     config
 );
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+fs.readdirSync(__dirname)
+    .filter(file => {
+        return (
+            file.indexOf(".") !== 0 &&
+            file !== basename &&
+            file.slice(-3) === ".js"
+        );
+    })
+    .forEach(file => {
+        const model = sequelize["import"](path.join(__dirname, file));
+        db[model.name] = model;
+    });
 
 Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
+    if (db[modelName].associate) {
+        db[modelName].associate(db);
+    }
 });
 
 db.sequelize = sequelize;
