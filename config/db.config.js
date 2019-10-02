ca1a4fbe6506712ddaf354a6bde923876f68ac36
Juan Carlos <juanksoliveira@gmail.com>
2019-10-02 00:10:01 -0300

    Set dotenv variables for db config

--- config/db.config.js
+++ config/db.config.js
@@ -1,9 +1,13 @@
+require("dotenv").config({
+    path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+});
+
 module.exports = {
-    host: "localhost",
-    username: "postgres",
-    password: "root",
-    database: "home_t",
-    dialect: "postgres",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    dialect: process.env.DB_DIALECT || "postgres",
     operatorsAliases: false,
     logging: false,
     define: {
