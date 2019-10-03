9d95ce001a0f767567704b7d2ed7154cb0e4fc7a
Juan Carlos <juanksoliveira@gmail.com>
2019-10-02 21:17:46 -0300

    Change routes and middlewares calls to app.js

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 
+const app = require("./app");
+
 const server = express();
 
 server.use(cors());
 
-const route = require("./routes");
-server.use(route);
 
 server.listen(3333);
