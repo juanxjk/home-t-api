1ed8fc39f4b0628db9381a149e1f92f4d66643e2
Juan Carlos <juanksoliveira@gmail.com>
2019-10-02 22:17:40 -0300

    Add initial session route and controller

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const routes = require("express").Router();
+const SessionController = require("./controllers/SessionController");
 
 routes.get("/", (req, res) => {
     return res.send("Hello World");
 });
 
+routes.post("/sessions", SessionController.store);
 module.exports = routes;
