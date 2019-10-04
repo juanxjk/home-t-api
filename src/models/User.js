c0cc2bc3226c07ad4144648acd787df930785d35
Juan Carlos <juanksoliveira@gmail.com>
2019-10-04 00:03:48 -0300

    Add JWT Token generation in response body

--- src/models/User.js
+++ src/models/User.js
@@ -1,13 +1,14 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define(
         "User",
         {
-        name: DataTypes.STRING,
-        email: DataTypes.STRING,
+            name: DataTypes.STRING,
+            email: DataTypes.STRING,
             password: DataTypes.VIRTUAL,
-        password_hash: DataTypes.STRING
+            password_hash: DataTypes.STRING
         },
         {
             hooks: {
@@ -27,5 +28,8 @@ module.exports = (sequelize, DataTypes) => {
         return bcrypt.compare(password, this.password_hash);
     };
 
+    User.prototype.generateToken = function() {
+        return jwt.sign({ id: this.id }, process.env.APP_SECRET);
+    };
     return User;
 };
