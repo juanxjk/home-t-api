cdaaac95c82f7cad2116ebff522efcd01647ff94
Juan Carlos <juanksoliveira@gmail.com>
2019-10-03 23:16:41 -0300

    Add unit test on User to compare password hash

---
+++ __tests__/unit/user.test.js
@@ -0,0 +1,22 @@
+const { User } = require("../../src/models");
+const bcrypt = require("bcryptjs");
+const truncate = require("../utils/truncate");
+
+describe("User", () => {
+    beforeEach(async () => {
+        await truncate();
+    });
+
+    it("should encrypt user password", async () => {
+        const user = await User.create({
+            name: "Foo Bar",
+            email: "foo@bar.com",
+            password: "123456"
+        });
+
+        const hash = await bcrypt.hash("123456", 8);
+        const compareHash = await bcrypt.compare("123456", hash);
+
+        expect(compareHash).toBe(true);
+    });
+});
