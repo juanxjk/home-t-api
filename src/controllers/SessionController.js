e3ea61f434cce41477d357e41ce46669a7489f1e
Juan Carlos <juanksoliveira@gmail.com>
2019-10-03 23:51:15 -0300

    Add SessionController User findOne by Email

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,5 +1,14 @@
+const { User } = require("../models");
 class SessionController {
     async store(req, res) {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            return res.status(401).json({ message: "User not found" });
+        }
+
         return res.status(200).send();
     }
 }
